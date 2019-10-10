020e48122ecf63088222208f4a4481adb36629ed
cindyzhu0830 <45746066+cindyzhu0830@users.noreply.github.com>
2019-10-10 11:06:11 -0400

    added graph printing (2nd graph need to be fixed)

--- JavaWorkspace/ICS4U1/Maze/src/app/Maze.java
+++ JavaWorkspace/ICS4U1/Maze/src/app/Maze.java
@@ -10,39 +10,31 @@ public class Maze {
     static List<Integer> resulty = new LinkedList<>();
     static int level[][], prex[][], prey[][];
     static int r, c, cx, cy;
+    static char[][] ch, copych;
 
     public static void main(String[] args) {
-        int sx = 0, sy = 0;
         Scanner sc = new Scanner(System.in);
-        r = sc.nextInt();
-        c = sc.nextInt();
+        r = 7;
+        c = 11;
         level = new int[r][c];
         prex = new int[r][c];
         prey = new int[r][c];
-        char[][] ch = new char[r][c];
+        ch = new char[r][c];
+        copych = ch.clone();
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
                 ch[i][j] = sc.next().charAt(0);
-                if (ch[i][j] == 'R') {
-                    sx = i;
-                    sy = j;
-                }
             }
         }
+        System.out.println("where do you want to drop the rat? (input 2d coordinates");
+        int sx = sc.nextInt();
+        int sy = sc.nextInt();
+
         boolean vis[][] = new boolean[r][c];
         System.out.println(search(sx, sy, vis, ch, 0, 'C'));
-        int copycx = cx, copycy = cy;
-        while(copycx!=sx||copycy!=sy){
-            resultx.add(copycx);
-            resulty.add(copycy);
-            int tcx = copycx;
-            copycx = prex[copycx][copycy];
-            copycy = prey[tcx][copycy];
-        }
-        while(!resultx.isEmpty()){
-            System.out.print(resultx.remove(resultx.size()-1) + " " + resulty.remove(resulty.size()-1) + ", ");
-        }
-        System.out.println();
+        printPath(cx, cy, sx, sy);
+        draw(copych);
+        printGraph(copych);
 
         sx = cx;
         sy = cy;
@@ -51,20 +43,17 @@ public class Maze {
         q1.clear();
         q2.clear();
         System.out.println(search(cx, cy, vis2, ch, 0, 'X'));
-        while(cx!=sx||cy!=sy){
-            resultx.add(cx);
-            resulty.add(cy);
-            int tcx = cx;
-            cx = prex[cx][cy];
-            cy = prey[tcx][cy];
-        }
-        while(!resultx.isEmpty()){
-            System.out.print(resultx.remove(resultx.size()-1) + " " + resulty.remove(resulty.size()-1) + ", ");
-        }
-        System.out.println();
-        
+        printPath(cx, cy, sx, sy);
+        draw(ch);
+        printGraph(ch);
 
     }
+
+
+
+
+
+
 
     static int search(int x, int y, boolean vis[][], char[][] ch, int lv, char target) {
         level[x][y] = lv;
@@ -81,7 +70,7 @@ public class Maze {
             if (!vis[x - 1][y] && ch[x - 1][y] != 'B') {
                 level[x - 1][y] = lv + 1;
                 prex[x - 1][y] = x;
-                prey[x-1][y] = y;
+                prey[x - 1][y] = y;
                 q1.add(x - 1);
                 q2.add(y);
 
@@ -89,22 +78,22 @@ public class Maze {
             if (!vis[x + 1][y] && ch[x + 1][y] != 'B') {
                 level[x + 1][y] = lv + 1;
                 prex[x + 1][y] = x;
-                prey[x+1][y] = y;
+                prey[x + 1][y] = y;
                 q1.add(x + 1);
                 q2.add(y);
             }
             if (!vis[x][y - 1] && ch[x][y - 1] != 'B') {
                 level[x][y - 1] = lv + 1;
-                prex[x][y-1] = x;
-                prey[x][y-1] = y;
+                prex[x][y - 1] = x;
+                prey[x][y - 1] = y;
                 q1.add(x);
                 q2.add(y - 1);
 
             }
             if (!vis[x][y + 1] && ch[x][y + 1] != 'B') {
                 level[x][y + 1] = lv + 1;
-                prex[x][y+1] = x;
-                prey[x][y+1] = y;
+                prex[x][y + 1] = x;
+                prey[x][y + 1] = y;
                 q1.add(x);
                 q2.add(y + 1);
 
@@ -113,6 +102,37 @@ public class Maze {
         int t1 = q1.poll();
         int t2 = q2.poll();
         return search(t1, t2, vis, ch, level[t1][t2], target);
+    }
+
+    public static void printPath(int curX, int curY, int tarX, int tarY) {
+        while (curX != tarX || curY != tarY) {
+            resultx.add(curX);
+            resulty.add(curY);
+            int tcx = curX;
+            curX = prex[curX][curY];
+            curY = prey[tcx][curY];
+        }
+    }
+
+    public static void printGraph(char[][] chList) {
+        for (int i = 0; i < chList.length; i++) {
+            for (int j = 0; j < chList[0].length; j++) {
+                System.out.print(chList[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void draw(char[][] chList) {
+        while (!resultx.isEmpty()) {
+            int tx = resultx.remove(resultx.size() - 1);
+            int ty = resulty.remove(resulty.size() - 1);
+            if (prex[tx][ty] == tx && chList[tx][ty] != 'C' && chList[tx][ty] != 'X') {
+                chList[tx][ty] = '-';
+            } else if (chList[tx][ty] != 'C' && chList[tx][ty] != 'X') {
+                chList[tx][ty] = '|';
+            }
+        }
     }
 
 }
