c90eeea9532578f7db03fa0026198be723345b00
cindyzhu0830 <45746066+cindyzhu0830@users.noreply.github.com>
2019-10-10 14:08:14 -0400

    oop

--- JavaWorkspace/ICS4U1/Maze/src/app/Maze.java
+++ JavaWorkspace/ICS4U1/Maze/src/app/Maze.java
@@ -1,16 +1,13 @@
 package app;
-
 import java.util.*;
-//git push test
+import java.io.File;
 
 public class Maze {
-    static Queue<Integer> q1 = new LinkedList<>();
-    static Queue<Integer> q2 = new LinkedList<>();
-    static List<Integer> resultx = new LinkedList<>();
-    static List<Integer> resulty = new LinkedList<>();
-    static int level[][], prex[][], prey[][];
-    static int r, c, cx, cy;
+    static Queue<Integer> q1, q2 = new LinkedList<>();
+    static List<Integer> resultx, resulty = new LinkedList<>();
+    static int level[][], prex[][], prey[][], r, c, cx, cy;
     static char[][] ch, copych;
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -21,54 +18,41 @@ public class Maze {
         prey = new int[r][c];
         ch = new char[r][c];
         copych = new char[r][c];
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                ch[i][j] = sc.next().charAt(0);
-                copych[i][j] = ch[i][j];
-            }
-        }
-        System.out.println("where do you want to drop the rat? (input 2d coordinates");
-        int sx = sc.nextInt();
-        int sy = sc.nextInt();
-
         boolean vis[][] = new boolean[r][c];
-        System.out.println(search(sx, sy, vis, ch, 0, 'C'));
-        printPath(cx, cy, sx, sy);
-        draw(copych);
+        boolean vis2[][] = new boolean[r][c];
+        readIn();
+        System.out.println("where do you want to drop the rat? (input 2d coordinates)");
+        int sx = sc.nextInt(), sy = sc.nextInt();
+        System.out.println("Shortest path to cheese: " + search(sx, sy, vis, ch, 0, 'C'));
         printGraph(copych);
         System.out.println();
-
         sx = cx;
         sy = cy;
-
-        boolean vis2[][] = new boolean[r][c];
         q1.clear();
         q2.clear();
-        System.out.println(search(cx, cy, vis2, copych, 0, 'X'));
-        printPath(cx, cy, sx, sy);
-        printGraph(ch);
-        System.out.println();
-        draw(ch);
+        System.out.println("Shortest path to exit: " + search(cx, cy, vis2, copych, 0, 'X'));
         printGraph(ch);
 
     }
 
+    static void readIn(){
+        File maze = new File("C:\\HighSchool\\UHS\\maze.txt");
+        Scanner fsc = new Scanner(maze);
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                ch[i][j] = fsc.next().charAt(0);
+                copych[i][j] = ch[i][j];
+            }
+        }
 
-
-
-
-
+    }
 
     static int search(int x, int y, boolean vis[][], char[][] ch, int lv, char target) {
-        level[x][y] = lv;
         vis[x][y] = true;
-
         if (ch[x][y] == target) {
-
             cx = x;
             cy = y;
             return lv;
-
         }
         if (x < r - 1 && y < c - 1) {
             if (!vis[x - 1][y] && ch[x - 1][y] != 'B') {
@@ -103,9 +87,8 @@ public class Maze {
 
             }
         }
-        int t1 = q1.poll();
-        int t2 = q2.poll();
-        return search(t1, t2, vis, ch, level[t1][t2], target);
+
+        return search(q1.poll(), q2.poll(), vis, ch, level[t1][t2], target);
     }
 
     public static void printPath(int curX, int curY, int tarX, int tarY) {
@@ -119,6 +102,7 @@ public class Maze {
     }
 
     public static void printGraph(char[][] chList) {
+        draw(chList);
         for (int i = 0; i < chList.length; i++) {
             for (int j = 0; j < chList[0].length; j++) {
                 System.out.print(chList[i][j] + " ");
@@ -127,7 +111,7 @@ public class Maze {
         }
     }
 
-    public static void draw(char[][] chList) {
+    public static void draw(char[][] chList) { //mutator that changes characters in the graph to draw the path
         while (!resultx.isEmpty()) {
             int tx = resultx.remove(resultx.size() - 1);
             int ty = resulty.remove(resulty.size() - 1);
@@ -136,6 +120,17 @@ public class Maze {
             } else if (chList[tx][ty] != 'C' && chList[tx][ty] != 'X') {
                 chList[tx][ty] = '|';
             }
+        }
+    }
+
+    public static void push(int x, int y, boolean[][] visi, int lev, int dx, int dy){
+        if(!visi[x][y]&&ch[x][y]!='B'){
+            level[x][y] = lev+1;
+            prex[x][y] = x-dx;
+            prey[x][y] = y-dy;
+            q1.add(x);
+            q2.add(y);
+
         }
     }
 
